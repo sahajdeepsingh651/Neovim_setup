7163bdb9874d28c7a263aa54648e86691603fd87
Sahaj <sahajdeepsingh100@gmail.com>
2025-07-07 08:15:11 +0530

    Added somemore files

--- init.lua
+++ init.lua
@@ -17,23 +17,10 @@ rtp:prepend(lazypath)
 
 
 require('lazy').setup({
-	{	
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    -- fill any relevant options here
-  },
-},
-{import = "plugins"},
-
+require 'plugins.bufferline',	
+require 'plugins.colorscheme',
+require 'plugins.neotree',
+require 'plugins.lualine',
+require 'plugins.treesitter'
 }
 )
